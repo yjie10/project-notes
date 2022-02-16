fb96d1efd7b2380974ad03db4b403a1c3d3d6e02
yjie10 <yy.jie28@gmail.com>
2022-02-15 16:59:58 -0800

    async test p1 set up & fetching data

---
+++ junior-to-senior/testing-jest/script2.js
@@ -0,0 +1,26 @@
+// asynchronous tests
+
+// fetch() in node won't work as it is something implemented by the browser and is on the window object; 
+// in node, there's no window object, and thus there's no window.fetch
+// therefore we need to install node-fetch
+
+const fetch = require('node-fetch');
+
+const getPokemonPromise = (fetch) => {
+  return fetch('https://pokeapi.co/api/v2/pokemon?limit=60&offset=60')
+    .then(resp => resp.json())
+    .then(res => console.log(res.results))
+}
+
+const getPokemon = async (fetch) => {
+  const getRequest = await fetch('https://pokeapi.co/api/v2/pokemon?limit=60&offset=60');
+  const data = await getRequest.json();
+  return data;
+}
+
+console.log(getPokemon(fetch));
+
+module.exports = {
+  getPokemonPromise,
+  getPokemon
+}
---
+++ junior-to-senior/testing-jest/script2.test.js
@@ -0,0 +1,3 @@
+const fetch = require('node-fetch');
+const pokemon = require('./script2');
+
